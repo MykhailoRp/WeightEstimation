8ca36a8410fa2685b18f006d64389ac150454635
Mykhailo Razbeiko <mykhailo.razb@gmail.com>
2026-02-21 13:25:24 +0200

    Postgres compose

--- backend/packages/api/src/api/main.py
+++ backend/packages/api/src/api/main.py
@@ -5,6 +5,6 @@ from api.model.basic import HealthCheck
 app = FastAPI()
 
 
-@app.get("/health/", status_code=200, include_in_schema=False)
+@app.get("/health", status_code=200, include_in_schema=False)
 def health_check() -> HealthCheck:
-    return HealthCheck()
+    return HealthCheck(status=200, text="Service api is healthy")
