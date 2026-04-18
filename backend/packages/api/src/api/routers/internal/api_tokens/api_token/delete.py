5d19ed92ea5226e9b027595f95b7921b1226d1a4
Mykhailo Razbeiko <mykhailo.razb@gmail.com>
2026-04-18 10:08:30 +0300

    FastAPI connection from frontend

--- backend/packages/api/src/api/routers/internal/api_tokens/api_token/delete.py
+++ backend/packages/api/src/api/routers/internal/api_tokens/api_token/delete.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from fastapi import APIRouter, HTTPException, Query, status
+from fastapi import APIRouter, HTTPException, Path, status
 
 from api.dependencies import DBSession, TokenData
 from api.models.api_token import ApiTokenDetailsResponse
@@ -13,12 +13,12 @@ router = APIRouter()
 
 @router.delete("/", status_code=200, operation_id="Delete Api Token")
 async def delete_token(
-    token: Annotated[ApiTokenStr, Query()],
+    api_token: Annotated[ApiTokenStr, Path()],
     session_maker: DBSession,
     token_data: TokenData,
 ) -> ApiTokenDetailsResponse:
     async with session_maker() as session:
-        deleted_token = await delete_token_script(session, token=token, customer_id=None if token_data.is_(UserRole.ADMIN) else token_data.id)
+        deleted_token = await delete_token_script(session, token=api_token, customer_id=None if token_data.is_(UserRole.ADMIN) else token_data.id)
 
         if deleted_token is None:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Token not found")
